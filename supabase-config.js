07f6f7f56c70c9d313d351b52e5dd728ecd3bf63
Adam0008 <168303248+Adam0008@users.noreply.github.com>
2026-02-16 18:50:10 +0100

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,13 +1,15 @@
+// =====================================================
+// CONFIGURATION SUPABASE
+// =====================================================
+
 const SUPABASE_URL = "https://bmxonrzyrymmwlweykie.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_qXghUgFuoBxwyJfs5Qg2vw_cbolYFvS";
 
-// On crée un client propre
+// On récupère createClient depuis la librairie CDN
 const supabaseClient = window.supabase.createClient(
   SUPABASE_URL,
   SUPABASE_ANON_KEY
 );
 
-// On le rend global sous un autre nom
+// On le rend global
 window.supabaseClient = supabaseClient;
-
-console.log("✅ Supabase connecté !");
